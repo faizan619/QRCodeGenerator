418a78978ec45d97f2b82e145c3de893b0f381d9
faizan alam <alamf6023@gmail.com>
2023-09-18 16:54:44 +0530

    added the new file name landing page for connecting reader and generator

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import './index.css'
+import Reader from './pages/Reader.jsx'
+import Landing from './Landing.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Reader/> */}
+    <Landing/>
   </React.StrictMode>,
 )
